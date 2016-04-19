bcbc02efda46a631e4d33407b21214b887200d6c
narayana14 <narayanawijaya@gmail.com>
2016-04-19 18:00:36 +0800

    commit for deploy to heroku

--- server.js
+++ server.js
@@ -9,10 +9,15 @@ var result = {};
 
 app.get('/api/whoami', function(req, res){
 	var address = os.networkInterfaces();
-	result.ipaddress = address['Wi-Fi'][1].address;
+	//result.ipaddress = address['Wi-Fi'][1].address;
 	result.software = os.type();
 	res.send(result);
 })
+
+dns.lookup('heroku.com', function(err, addresses, family){
+	console.log(addresses);
+	result.ipaddress = addresses;
+});
 
 app.listen(app.get('port'), function(){
 	console.log('Application start at localhost:5000');
