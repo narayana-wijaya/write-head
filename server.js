62100040b6de8630b0a7d7fc61772936eb798536
narayana14 <narayanawijaya@gmail.com>
2016-04-19 21:24:20 +0800

    make it better

--- server.js
+++ server.js
@@ -9,10 +9,11 @@ app.set('port', (process.env.PORT || 5000));
 var result = {};
 
 app.get('/api/whoami', function(req, res){
-	//result.ipaddress = req.ip;
+	var lang = req.headers["accept-language"];
 	var address = os.networkInterfaces();
 	result.ipaddress = address['eth0'][0].address;
-	result.software = os.type();
+	result.language = lang.slice(0, 5);
+	result.software = os.type().concat('; '+os.arch());
 	res.send(result);
 })
 
