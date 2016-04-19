b014eebfc90f470eb88fa98e859092dacf3659e1
narayana14 <narayanawijaya@gmail.com>
2016-04-19 21:37:41 +0800

    make it better

--- server.js
+++ server.js
@@ -1,19 +1,16 @@
 var express = require('express');
 var app = express();
-var url = require('url');
-const dns = require('dns');
 var os = require('os');
 
 app.set('port', (process.env.PORT || 5000));
 
-var result = {};
-
 app.get('/api/whoami', function(req, res){
-	var lang = req.headers["accept-language"];
 	var address = os.networkInterfaces();
-	result.ipaddress = address['eth0'][0].address;
-	result.language = lang.slice(0, 5);
-	result.software = os.type().concat('; '+os.arch());
+	var result = {
+		"ipaddress" : address['eth0'][0].address,
+		"language"  : req.headers["accept-language"].slice(0, 5),
+		"software"  : os.type().concat('; '+os.arch())
+	};
 	res.send(result);
 })
 
